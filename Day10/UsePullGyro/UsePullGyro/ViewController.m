99934a72a80475254d8f3878d825bc38ddd50f71
xiaotuan <583168425@qq.com>
2020-01-29 16:06:28 +0800

    添加第10天=>UsePullGyro->Pull方式使用陀螺仪

---
+++ Day10/UsePullGyro/UsePullGyro/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  UsePullGyro
+//
+//  Created by 覃团业 on 2020/1/29.
+//  Copyright © 2020 覃团业. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
+
+@interface ViewController ()
+
+/*
+ 运动管理器
+ */
+@property (nonatomic, strong) CMMotionManager *motionMgr;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    /*
+     陀螺仪
+     */
+    // 1. 创建CMMotionManager对象
+    self.motionMgr = [CMMotionManager new];
+    
+    // 2. 判断陀螺仪是否可用
+    if (![self.motionMgr isGyroAvailable]) {
+        return;
+    }
+    
+    // 3. 设置采样间隔，单位是秒
+    self.motionMgr.gyroUpdateInterval = 1;
+    
+    // 4. 开始采样
+    [self.motionMgr startGyroUpdates];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // 5. 获取data中的数值
+    CMRotationRate rotationRate = self.motionMgr.gyroData.rotationRate;
+    
+    NSLog(@"x: %f, y: %f, z: %f", rotationRate.x, rotationRate.y, rotationRate.z);
+}
+
+@end
